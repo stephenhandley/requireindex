7fcea78f8fd6a3a7a85659ad7c40a945238fff66
Stephen Handley <stephen.handley@gmail.com>
2012-05-06 02:41:50 -0700

    blame coffee

--- index.js
+++ index.js
@@ -1,26 +1,28 @@
-var fs   = require('fs'),
-    path = require('path');
+(function() {
+  var fs   = require('fs'),
+      path = require('path');
 
-module.exports = function(dir, basenames) {
-  var requires = {};
+  module.exports = function(dir, basenames) {
+    var requires = {};
   
-  if (arguments.length === 1) {  
-    fs.readdirSync(dir).forEach(function(filename) {
+    if (arguments.length === 1) {  
+      fs.readdirSync(dir).forEach(function(filename) {
   
-      if ((filename === 'index.js') || (filename[0] === '_')) { return; }
+        if ((filename === 'index.js') || (filename[0] === '_')) { return; }
     
-      filename = path.basename(filename, path.extname(filename));
-      var filepath = path.join(dir, filename);
+        filename = path.basename(filename, path.extname(filename));
+        var filepath = path.join(dir, filename);
     
-      requires[filename] = require(filepath);
-    });
+        requires[filename] = require(filepath);
+      });
   
-  } else {
-    basenames.forEach(function(basename) {
-      var filepath = path.join(dir, basename);
-      requires[basename] = require(filepath);
-    });
-  }
+    } else {
+      basenames.forEach(function(basename) {
+        var filepath = path.join(dir, basename);
+        requires[basename] = require(filepath);
+      });
+    }
   
-  return requires;
-};
+    return requires;
+  };
+})();
