a916c8b81eda5407833436504c03ece1f2bf9467
uzlopak <aras.abbasi@googlemail.com>
2022-11-21 10:28:59 +0100

    remove asserts dev dependency

--- test/test.js
+++ test/test.js
@@ -1,43 +1,25 @@
-var Assert = require('assert');
-var Asserts = require('asserts');
+'use strict'
 
-Asserts(function () {
-  var lib = require('./lib');
+var assert = require('assert');
+var lib = require('./lib');
 
-  return {
-    "requireindex should": {
-      "properly include files parallel to index.js and maintain structure": function () {
-        Asserts.all.equal([
-          [lib.bam.m,            [], "ok"],
-          [lib.bar.f,            [], "yea"],
-          [lib.bar.fing,         [], 'definitely'],
-          [lib.Foo.l,            [], 'yes'],
-          [lib.Foo.ls,           [], 'yep'],
-          [lib.bam.n,            [], 'ack'],
-          [lib.bar.fed.again,    [], 'again'],
-          [lib.bar.fed.somemore, [], 'somemore']
-        ]);
-      },
+assert.equal(lib.bam.m(), 'ok', 'requireindex should properly include files parallel to index.js and maintain structure');
+assert.equal(lib.bar.f(), 'yea', 'requireindex should properly include files parallel to index.js and maintain structure');
+assert.equal(lib.bar.fing(), 'definitely', 'requireindex should properly include files parallel to index.js and maintain structure');
+assert.equal(lib.Foo.l(), 'yes', 'requireindex should properly include files parallel to index.js and maintain structure');
+assert.equal(lib.Foo.ls(), 'yep', 'requireindex should properly include files parallel to index.js and maintain structure');
+assert.equal(lib.bam.n(), 'ack', 'requireindex should properly include files parallel to index.js and maintain structure');
+assert.equal(lib.bar.fed.again(), 'again', 'requireindex should properly include files parallel to index.js and maintain structure');
+assert.equal(lib.bar.fed.somemore(), 'somemore', 'requireindex should properly include files parallel to index.js and maintain structure');
 
-      "ignore _ prefixed files": function () {
-        Assert.equal(('_private' in lib), false);
-      },
+assert.equal('_private' in lib, false, 'ignore _ prefixed files');
 
-      "not include files not mentioned when second array argument is used": function () {
-        Assert.equal(('ignored' in lib.bar.fed), false);
-      },
+assert.equal('ignored' in lib.bar.fed, false, 'not include files not mentioned when second array argument is used');
 
-      "ignore non javascript files": function () {
-        Assert.equal(('not_javascript' in lib), false);
-      },
+assert.equal('not_javascript' in lib, false, 'ignore non javascript files');
 
-      "sort files by lowercase alpha of the filename": function () {
-        Assert.equal(Object.keys(lib)[0], 'bam');
-      },
-      
-      "ignore dot files": function () {
-        Assert.equal(('.also_private' in lib), false);
-      },
-    }
-  };
-});
+assert.equal(Object.keys(lib)[0], 'bam', 'sort files by lowercase alpha of the filename /1');
+assert.equal(Object.keys(lib)[1], 'bar', 'sort files by lowercase alpha of the filename /2');
+assert.equal(Object.keys(lib)[2], 'Foo', 'sort files by lowercase alpha of the filename /3');
+
+assert.equal('.also_private' in lib, false, 'ignore dot files');
